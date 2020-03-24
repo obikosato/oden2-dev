9d44d5efdef46165deba431f6375f656305d13f8
obiko.satoh <Obiko.Sato@jp.ricoh.com>
2020-03-24 17:39:58 +0900

    LineNotifyの部分追加

---
+++ Common/Tools/LineNotify/ILineMessenger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Tools.LineNotify
+{
+    interface ILineMessenger
+    {
+        public bool SendMessage(string accessToken, string message);
+    }
+}
---
+++ Common/Tools/LineNotify/LineMessenger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Common.Tools.LineNotify
+{
+    class LineMessenger : ILineMessenger
+    {        
+        private readonly string pythonInterpreterPath = @"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe";
+        private readonly string pythonScriptPath;
+
+        public LineMessenger(string pyscript = @"C:\Users\z00s600151\source\repos\BlazorApp0124\BlazorApp0124.Models\Tools\LineMessenger.py")
+        {
+            pythonScriptPath = pyscript;
+        }
+        public bool SendMessage(string accessToken, string message)
+        {
+            if (File.Exists(pythonScriptPath))
+            {
+                var arguments = new List<string>
+                {
+                    pythonScriptPath,
+                    accessToken,
+                    message.Replace(" ", "　")
+                };
+
+                var process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo(pythonInterpreterPath)
+                    {
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        Arguments = string.Join(" ", arguments),
+                    },
+                };
+
+                try
+                {
+                    process.Start();
+
+                    var sr = process.StandardOutput;
+                    string status = sr.ReadLine();
+
+                    process.WaitForExit();
+                    process.Close();
+
+                    return Equals(status, "200");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Generic Exception Handler: {e}");
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
