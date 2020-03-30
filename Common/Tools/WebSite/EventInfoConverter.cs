ff3dd1a7bafc029e1a1515bd999642ffc1961385
epnt24 <epnt24@gamil.com>
2020-03-30 13:26:23 +0900

    trivial

--- Common/Tools/WebSite/EventInfoConverter.cs
+++ Common/Tools/WebSite/EventInfoConverter.cs
@@ -10,7 +10,7 @@ namespace Common.Tools.WebSite
         private readonly string pythonInterpreterPath = @"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe";
         private readonly string pythonScriptPath;
 
-        public EventInfoConverter(string pyscript = @"C:oden2-dev\Common\Tools\WebSite\GetEventInfoJson.py") {
+        public EventInfoConverter(string pyscript = @"C:\oden2-dev\Common\Tools\WebSite\GetEventInfoJson.py") {
             pythonScriptPath = pyscript;
         }
 
