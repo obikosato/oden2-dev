e0233f46265343e7ea63c4bafe8870d0d9b74e52
epnt24 <epnt24@gamil.com>
2020-03-30 12:35:39 +0900

    ログ用のクラス追加

---
+++ Log/ILogger.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Log
+{
+    public interface ILogger
+    {
+        public void Log(string id, string message);
+    }
+}
---
+++ Log/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Log
+{
+    public class Logger : ILogger
+    {
+        public void Log(string id = "None", string message = "No data.")
+        {
+            string path = @"oden_log.csv";
+            using StreamWriter sw = File.AppendText(path);
+            sw.WriteLine("{0},{1},{2}", DateTime.Now, id, message);
+        }
+    }
+}
